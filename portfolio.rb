23e52dcc557242732c55f11d8bdbc483c63d141f
Johannes Held <mail@hehejo.de>
2011-08-24 00:11:48 +0200

    Calculate and print the portfolio

--- portfolio.rb
+++ portfolio.rb
@@ -27,15 +27,51 @@ def load_portfolio
 	return YAML.load(File.read(PORTFOLIO))
 end
 
-if __FILE__ == $0
-	portfolio = load_portfolio
+# construct the quote list for querying the stock information
+# @parm portfolio the portfolio (Array loaded from the dataset)
+# @return [String] comma-separted stock names
+def get_quote_list(portfolio = Array.new)
 	quote_list = Array.new
 	portfolio.each do |p|
 		quote_list << p['stock']
 	end
+	return quote_list.join(',')
+end
 
-	qs = get_quote_info(quote_list.join(','))
-	qs.each do |s,v|
-		puts "#{s}:\t#{v.lastTrade}"
+# calculates profit for the given portfolio
+# @parm portfolio the portfolio (array with hashes loaded from the dataset)
+# @return [Hash] updated with profitinformation query with "profit_percent" and "profit_amount" 
+def calculate_portfolio(portfolio = Array.new)
+	qs = get_quote_info(get_quote_list(portfolio))
+	portfolio.each do |p|
+		p['info'] = qs[p['stock']]
+		value_orderd = p['amount']*p['price']
+		value_now = p['amount']*p['info'].lastTrade
+		p['profit_amount'] = value_now - value_orderd
 	end
+	return portfolio
+end
+
+# printing the actual portfolio
+# @parm portfolio the portfolio (array with hashes loaded from the dataset)
+def print_portfolio(portfolio = Array.new)
+	template=<<EOT
+STOCK:\tLASTTRADE
+\tAMOUNT @ PRICE -> PROFIT
+EOT
+	portfolio.each do |p|
+		t = template.dup
+		t.sub!('STOCK', p['stock'])
+		t.sub!('LASTTRADE', "#{p['info'].lastTrade}")
+		t.sub!('AMOUNT', p['amount'].to_s)
+		t.sub!('PRICE', p['price'].to_s)
+		t.sub!('PROFIT', p['profit_amount'].to_s)
+		puts t
+	end
+end
+
+if __FILE__ == $0
+	portfolio = calculate_portfolio load_portfolio
+	print_portfolio portfolio
+
 end
